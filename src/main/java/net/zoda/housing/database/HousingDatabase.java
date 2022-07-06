3851195ecb80c067c842239d90a6bc947d1bd7da
novastosha <galliigotu@gmail.com>
2022-07-06 11:09:51 +0100

    Added functionality methods

--- src/main/java/net/zoda/housing/database/HousingDatabase.java
+++ src/main/java/net/zoda/housing/database/HousingDatabase.java
@@ -1,10 +1,17 @@
 package net.zoda.housing.database;
 
 import com.mongodb.client.MongoDatabase;
+import net.zoda.housing.house.PlayerHouse;
+
+import java.util.UUID;
 
 public interface HousingDatabase {
 
     String getConnectionString();
     MongoDatabase getHousingDatabase();
+
+    void insertHouse(PlayerHouse house,PlayerHouse... houses);
+    PlayerHouse[] getHousesOf(UUID player);
+    PlayerHouse getHouse(UUID houseUUID);
 
 }
