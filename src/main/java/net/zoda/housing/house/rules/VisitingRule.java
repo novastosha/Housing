35290ac9844c4518fa425d9d34bc53ef22c15cce
novastosha <galliigotu@gmail.com>
2022-10-13 20:53:53 +0100

    Added rule color

--- src/main/java/net/zoda/housing/house/rules/VisitingRule.java
+++ src/main/java/net/zoda/housing/house/rules/VisitingRule.java
@@ -1,14 +1,16 @@
 package net.zoda.housing.house.rules;
 
 import lombok.RequiredArgsConstructor;
+import org.bukkit.ChatColor;
 
 @RequiredArgsConstructor
 public enum VisitingRule {
 
-    PUBLIC(true),
-    PRIVATE(true)
+    PUBLIC(true,ChatColor.GREEN),
+    PRIVATE(true,ChatColor.RED)
     ;
 
     public final boolean single;
+    public final ChatColor color;
 
 }
