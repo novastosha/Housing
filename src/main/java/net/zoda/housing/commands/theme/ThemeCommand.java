5f3b0da5fc0229d3644785c1fd3f34fc180209ff
novastosha <galliigotu@gmail.com>
2022-10-13 20:56:41 +0100

    Finished the theme system

--- src/main/java/net/zoda/housing/commands/theme/ThemeCommand.java
+++ src/main/java/net/zoda/housing/commands/theme/ThemeCommand.java
@@ -24,6 +24,7 @@ import net.zoda.api.command.argument.Argument;
 import net.zoda.api.command.argument.ArgumentType;
 import net.zoda.api.command.subcommand.Subcommand;
 import net.zoda.api.command.subcommand.group.SubcommandGroup;
+import net.zoda.housing.house.PlayerHouse;
 import net.zoda.housing.house.instance.LiveHouseInstance;
 import net.zoda.housing.house.theme.Theme;
 import net.zoda.housing.house.theme.ThemeEditSession;
@@ -31,6 +32,8 @@ import net.zoda.housing.plugin.HousingPlugin;
 import net.zoda.housing.utils.*;
 import net.zoda.housing.utils.loaders.TemporaryLoader;
 import org.bukkit.*;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -48,6 +51,7 @@ import java.util.*;
 import java.util.function.Function;
 
 import static net.zoda.housing.utils.Utils.apply;
+import static net.zoda.housing.utils.Utils.displayVisitingRules;
 
 @Command(name = "theme", permissions = "housing.themes", playerOnly = true)
 public class ThemeCommand implements ACommand, Listener {
@@ -74,14 +78,28 @@ public class ThemeCommand implements ACommand, Listener {
     });
     private static final Map<UUID, Pair<Location, Location>> schematicPositions = new HashMap<>();
     private static final Map<UUID, Pair<Location, Location>> spawnLocations = new HashMap<>();
+    private static final Map<UUID, Location> housePasteLocations = new HashMap<>();
+    private static final Map<UUID, Map<Theme.CoordinateType, Integer>> coordinates = new HashMap<>();
+
+    public static Map<Theme.CoordinateType, Integer> coordsFromConfigurationSection(ConfigurationSection coordinates) {
+        Map<Theme.CoordinateType, Integer> map = new HashMap<>();
+
+        for (Theme.CoordinateType type1 : Theme.CoordinateType.values()) {
+            if (coordinates.contains(type1.configName)) {
+                map.put(type1, coordinates.getInt(type1.configName));
+            }
+        }
+
+        return map;
+    }
 
     @EventHandler
     public void onInteract(PlayerInteractEvent interactEvent) {
         if (interactEvent.getClickedBlock() == null || !Theme.EDIT_SESSIONS.containsKey(interactEvent.getPlayer().getUniqueId()))
             return;
 
-        if(interactEvent.getItem() == null) return;
-        if(!interactEvent.getItem().getItemMeta().getPersistentDataContainer().has(wandKey)) return;
+        if (interactEvent.getItem() == null) return;
+        if (!interactEvent.getItem().getItemMeta().getPersistentDataContainer().has(wandKey)) return;
 
         PositionType positionType = interactEvent.getAction().isLeftClick() ? PositionType.FIRST : PositionType.SECOND;
         final Location location = interactEvent.getClickedBlock().getLocation();
@@ -91,10 +109,10 @@ public class ThemeCommand implements ACommand, Listener {
 
     @EventHandler
     public void onWorldChange(PlayerChangedWorldEvent event) {
-        if(!Theme.EDIT_SESSIONS.containsKey(event.getPlayer().getUniqueId())) return;
-        if(!event.getFrom().equals(Theme.EDIT_SESSIONS.get(event.getPlayer().getUniqueId()).world())) return;
+        if (!Theme.EDIT_SESSIONS.containsKey(event.getPlayer().getUniqueId())) return;
+        if (!event.getFrom().equals(Theme.EDIT_SESSIONS.get(event.getPlayer().getUniqueId()).world())) return;
 
-        endSession(event.getPlayer().getUniqueId(),true);
+        endSession(event.getPlayer().getUniqueId(), true);
     }
 
     @EventHandler
@@ -109,14 +127,18 @@ public class ThemeCommand implements ACommand, Listener {
         if (Theme.EDIT_SESSIONS.containsKey(uuid)) {
             ThemeEditSession editSession = Theme.EDIT_SESSIONS.get(uuid);
 
-            for(Player player : editSession.world().getPlayers()){
-                player.kickPlayer(ChatColor.RED+"Session ended!");
+            for (Player player : editSession.world().getPlayers()) {
+                player.kickPlayer(ChatColor.RED + "Session ended!");
             }
 
             Bukkit.getServer().unloadWorld(editSession.world(), false);
 
-            assert remove;
-            Theme.EDIT_SESSIONS.remove(uuid);
+            schematicPositions.remove(uuid);
+            housePasteLocations.remove(uuid);
+            spawnLocations.remove(uuid);
+            coordinates.remove(uuid);
+
+            if (remove) Theme.EDIT_SESSIONS.remove(uuid);
         }
     }
 
@@ -148,6 +170,8 @@ public class ThemeCommand implements ACommand, Listener {
     });
 
     private final Class<Material> item = Material.class;
+    private final Class<Theme.CoordinateType> coordinateTypeClass = Theme.CoordinateType.class;
+
     private final Function<Player, List<String>> schematic_file_name = player -> {
         List<String> list = new ArrayList<>();
         for (File file : Files.THEMES_DIRECTORY.getFile().listFiles()) {
@@ -177,11 +201,8 @@ public class ThemeCommand implements ACommand, Listener {
             @Argument(name = "name", type = ArgumentType.STRING),
             @Argument(name = "display_name", type = ArgumentType.STRING),
             @Argument(name = "item", type = ArgumentType.ENUM),
-            @Argument(name = "maximum_x", type = ArgumentType.INTEGER),
-            @Argument(name = "maximum_z", type = ArgumentType.INTEGER),
-            @Argument(name = "schematic_file_name", type = ArgumentType.STRING, required = false)
     })
-    public void createTheme(Player player, String name, String displayName, Material item, Integer maxX, Integer maxZ, String schematicFileName) {
+    public void createTheme(Player player, String name, String displayName, Material item) {
         File themeFile = new File(Files.THEMES_DIRECTORY.getFile(), name + ".yml");
         if (Theme.THEMES.containsKey(name) || themeFile.exists()) {
             player.sendMessage(Component.text("A theme with this name already exists!").color(NamedTextColor.RED));
@@ -193,21 +214,9 @@ public class ThemeCommand implements ACommand, Listener {
         configuration.set("code-name", name);
         configuration.set("item", item.name().toLowerCase(Locale.ROOT));
 
-
-        if (schematicFileName != null) {
-            final File file = new File(Files.THEMES_DIRECTORY.getFile(), schematicFileName);
-            if (!file.exists()) {
-                player.sendMessage(Component.text("Schematic file doesn't exist!").color(NamedTextColor.RED));
-                return;
-            }
-            configuration.set("schematic-file", file.getName());
-        }
-        configuration.set("plot.max-x", maxX);
-        configuration.set("plot.max-z", maxZ);
-
         try {
             configuration.save(themeFile);
-            Theme.THEMES.put(name, new Theme(name, ChatColor.translateAlternateColorCodes('&', displayName), item, null).setEnabled(false));
+            Theme.THEMES.put(name, new Theme(name, ChatColor.translateAlternateColorCodes('&', displayName), item, new byte[0], themeFile, null, null, null).setEnabled(false));
 
             player.sendMessage(Component.text("Successfully created this theme!").color(NamedTextColor.GREEN));
         } catch (IOException e) {
@@ -252,14 +261,18 @@ public class ThemeCommand implements ACommand, Listener {
             World world = Bukkit.getServer().getWorld(worldName);
             Location center = new Location(world, 0, 90, 0);
 
-            if (theme.getSchematic() == null) {
+            coordinates.put(player.getUniqueId(), Theme.MAXIMUM_COORDINATES);
+            spawnLocations.put(player.getUniqueId(), new Pair<>(
+                    theme.getNpcSpawn() == null ? null : theme.getNpcSpawn().toBukkitLocation(world),
+                    theme.getSpawn() == null ? null : theme.getSpawn().toBukkitLocation(world)));
+            if (theme.getPasteLocation() != null) {
+                housePasteLocations.put(player.getUniqueId(), theme.getPasteLocation().toBukkitLocation(world));
+            }
+            if (theme.getSchematic() == null || theme.getSchematic().length == 0) {
                 world.getBlockAt(center).setType(Material.STONE);
             } else {
-                ClipboardFormat format = ClipboardFormats.findByFile(theme.getSchematic());
-                if (format == null) {
-                    throw new LiveHouseInstance.UnableToLoadHouseException("Unable to detect schematic type of file: " + theme.getSchematic().getName());
-                }
-                try (ClipboardReader reader = format.getReader(new FileInputStream(theme.getSchematic()))) {
+                ClipboardFormat format = BuiltInClipboardFormat.FAST;
+                try (ClipboardReader reader = format.getReader(new ByteArrayInputStream(theme.getSchematic()))) {
                     Clipboard clipboard = reader.read();
 
                     center = new Location(world, 0, clipboard.getMinY(), 0);
@@ -276,6 +289,7 @@ public class ThemeCommand implements ACommand, Listener {
                         Operations.complete(operation);
                     }
                 }
+                spawnLocations.put(player.getUniqueId(), new Pair<>(theme.getNpcSpawn().toBukkitLocation(world), theme.getSpawn().toBukkitLocation(world)));
             }
 
             Location spawnLocation = center.toCenterLocation().add(0, 1, 0);
@@ -287,19 +301,18 @@ public class ThemeCommand implements ACommand, Listener {
 
             Utils.resetPlayer(player, GameMode.CREATIVE, Utils.ResetAttribute.ALL);
             Theme.EDIT_SESSIONS.put(player.getUniqueId(), new ThemeEditSession(theme, world, player.getUniqueId()));
-            player.teleport(spawnLocation);
 
+            player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Heads up! ALL houses will be pasted on (X+ y: Y± z: Z+)");
+            player.teleport(spawnLocation);
         } catch (WorldAlreadyExistsException e) {
             player.sendMessage(ChatColor.GREEN + "Couldn't create the temporary world, please try again!");
             e.printStackTrace();
             return;
-        } catch (LiveHouseInstance.UnableToLoadHouseException | IOException e) {
+        } catch (IOException e) {
             player.sendMessage(ChatColor.GREEN + "There was an error loading the schematic! please try again!");
 
             final World world = player.getServer().getWorld(worldName);
-
-            assert world != null;
-            player.getServer().unloadWorld(world, false);
+            if (world != null) player.getServer().unloadWorld(world, false);
         }
     }
 
@@ -312,6 +325,52 @@ public class ThemeCommand implements ACommand, Listener {
             player.sendMessage(ChatColor.RED + "Missing positions!");
             return;
         }
+
+        Pair<Location, Location> spawns = spawnLocations.getOrDefault(player.getUniqueId(), new Pair<>());
+
+        if (spawns.getA() == null || spawns.getB() == null) {
+            player.sendMessage(ChatColor.RED + "Missing spawn locations!");
+            return;
+        }
+
+        if (!housePasteLocations.containsKey(player.getUniqueId())) {
+            player.sendMessage(ChatColor.RED + "Missing house paste location!");
+            return;
+        }
+
+        for (Theme.CoordinateType coordinateType : Theme.CoordinateType.values()) {
+            if (coordinates.getOrDefault(player.getUniqueId(), new HashMap<>()).containsKey(coordinateType)) continue;
+            player.sendMessage(ChatColor.RED + "Missing coordinates!");
+            return;
+        }
+
+        File themeFile = session.theme().getFile();
+
+        try {
+
+            if (themeFile == null) {
+                themeFile = new File(Files.THEMES_DIRECTORY.getFile(), session.theme().getCodeName() + ".schem");
+                if (!themeFile.exists()) {
+                    themeFile.createNewFile();
+                }
+            }
+
+            YamlConfiguration configuration = YamlConfiguration.loadConfiguration(themeFile);
+
+            configuration.set("npc-spawn", WorldlessLocation.fromBukkitLocation(spawns.getA()));
+            configuration.set("spawn", WorldlessLocation.fromBukkitLocation(spawns.getB()));
+            configuration.set("coordinates", coordsConfigurationSection(coordinates.get(player.getUniqueId()), configuration.createSection("coordinates")));
+            configuration.set("house-paste-location", WorldlessLocation.fromBukkitLocation(housePasteLocations.get(player.getUniqueId())));
+            configuration.set("schematic-file", session.theme().getCodeName() + ".schem");
+            configuration.set("paste.first", WorldlessLocation.fromBukkitLocation(schematicPositions.get(player.getUniqueId()).getA()));
+            configuration.set("paste.second", WorldlessLocation.fromBukkitLocation(schematicPositions.get(player.getUniqueId()).getB()));
+
+            configuration.save(themeFile);
+        } catch (IOException e) {
+            HousingPlugin.getPluginLogger().severe("Couldn't save theme config: " + session.theme().getCodeName());
+            return;
+        }
+
 
         com.sk89q.worldedit.world.World world = BukkitAdapter.adapt(player.getWorld());
 
@@ -328,15 +387,23 @@ public class ThemeCommand implements ACommand, Listener {
 
                 Operations.complete(forwardExtentCopy);
 
-                try (ClipboardWriter writer = BuiltInClipboardFormat.FAST.getWriter(new FileOutputStream(session.theme().getCodeName()+".schem"))) {
+                try (ClipboardWriter writer = BuiltInClipboardFormat.FAST.getWriter(new FileOutputStream(new File(Files.THEMES_DIRECTORY.getFile(), session.theme().getCodeName() + ".schem")))) {
                     writer.write(clipboard);
                 } catch (IOException e) {
-                    player.sendMessage(ChatColor.RED+"Couldn't save theme!");
+                    player.sendMessage(ChatColor.RED + "Couldn't save theme!");
                     return;
                 }
             }
         }
-        endSession(player.getUniqueId(),true);
+        endSession(player.getUniqueId(), true);
+    }
+
+    public static ConfigurationSection coordsConfigurationSection(Map<Theme.CoordinateType, Integer> coordinateTypeIntegerMap, ConfigurationSection section) {
+        for (Map.Entry<Theme.CoordinateType, Integer> entry : coordinateTypeIntegerMap.entrySet()) {
+            section.set(entry.getKey().configName, entry.getValue());
+
+        }
+        return section;
     }
 
     @Subcommand(name = "exit")
@@ -348,8 +415,8 @@ public class ThemeCommand implements ACommand, Listener {
     @SubcommandGroup("edit")
     @Subcommand(name = "wand")
     public void giveWand(Player player) {
-        if(player.getInventory().contains(wandItem)) {
-            player.sendMessage(ChatColor.RED+"You already have the selection wand!");
+        if (player.getInventory().contains(wandItem)) {
+            player.sendMessage(ChatColor.RED + "You already have the selection wand!");
             return;
         }
 
@@ -361,33 +428,56 @@ public class ThemeCommand implements ACommand, Listener {
         SECOND
     }
 
+    @SubcommandGroup("edit")
+    @SubcommandGroup("set")
+    @Subcommand(name = "coordinate", arguments = {
+            @Argument(name = "type", type = ArgumentType.ENUM, completer = "coordinateTypeClass"),
+            @Argument(name = "value", type = ArgumentType.INTEGER)
+    })
+    public void setCoordinate(Player player, Theme.CoordinateType coordinateType, Integer value) {
+        Map<Theme.CoordinateType, Integer> coordinateMap = coordinates.getOrDefault(player.getUniqueId(), new HashMap<>());
+        coordinateMap.put(coordinateType, value);
+
+        coordinates.put(player.getUniqueId(), coordinateMap);
+        player.sendMessage(ChatColor.GREEN + "Successfully set the coordinate: " + coordinateType.name() + " to: " + value);
+    }
 
     @SubcommandGroup("edit")
     @SubcommandGroup("set")
-    @Subcommand(name = "spawn",arguments = @Argument(name = "location",type = ArgumentType.LOCATION,required = false))
-    public void setPlayerSpawn(Player player,Location location) {
-        if(location == null) location = player.getLocation();
+    @Subcommand(name = "paste-location", arguments = @Argument(name = "location", type = ArgumentType.LOCATION, required = false))
+    public void setHousePasteLocation(Player player, Location location) {
+        location = (location == null ? player.getLocation().toBlockLocation() : location.toBlockLocation());
+
+        housePasteLocations.put(player.getUniqueId(), location);
+        player.sendMessage(ChatColor.GREEN + "Successfully set paste location to: " + ArgumentType.LOCATION.stringify(player, location));
+    }
+
+    @SubcommandGroup("edit")
+    @SubcommandGroup("set")
+    @Subcommand(name = "spawn", arguments = @Argument(name = "location", type = ArgumentType.LOCATION, required = false))
+    public void setPlayerSpawn(Player player, Location location) {
+        if (location == null) location = player.getLocation();
 
         Pair<Location, Location> pair = spawnLocations.getOrDefault(player.getUniqueId(), new Pair<>());
 
         pair.setB(location);
 
-        spawnLocations.put(player.getUniqueId(),pair);
-        player.sendMessage(ChatColor.GREEN+"Successfully set the Player spawn location!");
+        spawnLocations.put(player.getUniqueId(), pair);
+        player.sendMessage(ChatColor.GREEN + "Successfully set the Player spawn location!");
     }
 
     @SubcommandGroup("edit")
     @SubcommandGroup("set")
-    @Subcommand(name = "npc-spawn",arguments = @Argument(name = "location",type = ArgumentType.LOCATION,required = false))
-    public void setNPCSpawn(Player player,Location location) {
-        if(location == null) location = player.getLocation();
+    @Subcommand(name = "npc-spawn", arguments = @Argument(name = "location", type = ArgumentType.LOCATION, required = false))
+    public void setNPCSpawn(Player player, Location location) {
+        if (location == null) location = player.getLocation();
 
         Pair<Location, Location> pair = spawnLocations.getOrDefault(player.getUniqueId(), new Pair<>());
 
         pair.setA(location);
 
-        spawnLocations.put(player.getUniqueId(),pair);
-        player.sendMessage(ChatColor.GREEN+"Successfully set the NPC spawn location!");
+        spawnLocations.put(player.getUniqueId(), pair);
+        player.sendMessage(ChatColor.GREEN + "Successfully set the NPC spawn location!");
     }
 
     @SubcommandGroup("edit")
@@ -401,7 +491,7 @@ public class ThemeCommand implements ACommand, Listener {
             case SECOND -> pair.setB(location.toBlockLocation());
         }
 
-        schematicPositions.put(player.getUniqueId(),pair);
+        schematicPositions.put(player.getUniqueId(), pair);
 
         player.sendMessage(ChatColor.GREEN + "Set the " + positionType.name().toLowerCase() + " position to: (" + ArgumentType.LOCATION.stringify(player, location) + ")");
     }
