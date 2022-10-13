fc0ec00a070b8cc7fc42428e97d8eb4d1f10f149
novastosha <galliigotu@gmail.com>
2022-10-13 20:53:37 +0100

    Added conversion methods

--- src/main/java/net/zoda/housing/utils/WorldlessLocation.java
+++ src/main/java/net/zoda/housing/utils/WorldlessLocation.java
@@ -1,5 +1,7 @@
 package net.zoda.housing.utils;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.configuration.serialization.SerializableAs;
 import org.jetbrains.annotations.NotNull;
@@ -7,7 +9,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.HashMap;
 import java.util.Map;
 
-@SerializableAs("location")
+
+@SerializableAs("WorldlessLocation")
 public record WorldlessLocation(double x, double y, double z) implements ConfigurationSerializable {
 
 
@@ -24,5 +27,12 @@ public record WorldlessLocation(double x, double y, double z) implements Configu
 
     public static WorldlessLocation deserialize(Map<String, Object> map) {
         return new WorldlessLocation((double) map.get("x"),(double) map.get("y"),(double) map.get("z"));
+    }
+
+    public Location toBukkitLocation(World world) {
+        return new Location(world,x,y,z);
+    }
+    public static WorldlessLocation fromBukkitLocation(Location location) {
+        return new WorldlessLocation(location.getX(),location.getY(),location.getZ());
     }
 }
